aa2551a62dfbaa5becb3531c99485968a7c8a033
valdi-korot <valdi-korot@mail.ru>
2018-07-03 11:57:29 +0300

    Add project files.

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.SimpleEchoBot.sln -p:DeployOnBuild=true -p:PublishProfile=waiterbot-Web-Deploy.pubxml -p:Password=xjWZjB6mLEAjjd3RNb7h3GFnmeENf1ZZnE4wZRbzklXKJkYjjgPjLwstpuGR
+
